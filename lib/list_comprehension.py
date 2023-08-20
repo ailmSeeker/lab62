71382db16ab72eb1375e0b8d6be6cacf9f807880
Mohamed Yusuf <mohamed208.y@proton.me>
2023-08-20 17:58:26 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    new_list = [item for item in num_list if item % 2 == 0]
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+    squared_minus_one = [n + "!" for n in sentence_list]
+    return squared_minus_one
